dc9d9c3e398cfdf4c23ad776a8b1d1286df78e91
Brendan Rius <brendan@omixy.com>
2016-06-16 18:50:20 +0100

    Client can now connect and request rides

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,42 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+class Client {
+    constructor(id, socket) {
+        this.id = id;
+        this.socket = socket;
+    }
+}
+
+class Dispatcher {
+    constructor() {
+        this.clients = [];
+    }
+
+    newClient(socket) {
+        let client = new Client(this.clients.length, socket);
+        this.clients.push(client);
+        socket.on('request', request => {
+            console.log(`Client {client.id} requested "{request}"`);
+        });
+    }
+}
+
+let dispatcher = new Dispatcher();
 
 io.on('connection', function (socket) {
-    console.log('a user connected');
+    socket.on('identify', role => {
+        switch (role.toLowerCase()) {
+            case 'client':
+                dispatcher.newClient(socket);
+                break;
+            default:
+                throw new Error('Role does not exist');
+        }
+    });
 });
 
 http.listen(3000, function () {
-    console.log('listening on *:3000');
+    console.log('Listening on *:3000');
 });
